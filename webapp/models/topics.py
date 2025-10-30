597398e10666f671a4a82361041efd2f64f6949a
Aiigeriim <sardarbekova1005@gmail.com>
2025-10-30 21:54:59 +0300

    добавлять тему могут аутентифицированный юзер, связь темы с юзером(автор)

--- webapp/models/topics.py
+++ webapp/models/topics.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 from webapp.models import BaseCreateUpdateModel
@@ -6,6 +7,7 @@ from webapp.models import BaseCreateUpdateModel
 class Topic(BaseCreateUpdateModel):
     topic = models.CharField(verbose_name='Название', max_length=100)
     content = models.TextField(verbose_name='Содержимое')
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='topics',verbose_name='Автор')
 
     class Meta:
         db_table = 'topics'
--- webapp/views/topics.py
+++ webapp/views/topics.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView, DetailView
 
@@ -5,10 +6,14 @@ from webapp.forms import TopicForm
 from webapp.models import Topic
 
 
-class CreateTopic(CreateView):
+class CreateTopic(LoginRequiredMixin, CreateView):
     form_class = TopicForm
     template_name = 'topics/create_topic.html'
     success_url = reverse_lazy('webapp:topics_list')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 class TopicsList(ListView):
@@ -22,6 +27,11 @@ class TopicsList(ListView):
 class TopicDetail(DetailView):
     model = Topic
     template_name = 'topics/topic_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["answers"] = self.object.answers.order_by('created_at')
+        return context
 
 
 
